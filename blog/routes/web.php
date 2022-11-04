1a44727b7346827b4b70606058f045d4f7535602
finsanurpandi <finsaworks@gmail.com>
2022-11-04 12:29:12 +0700

    authentication and authorization

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,9 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/test',[TestController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::group([
+        'middleware' => 'role:lecture',
+        'prefix' => 'lecture',
+        'as' => 'lecture.'
+    ], function () {
+        Route::get('/', [HomeController::class, 'lecture'])->name('show');
+    });
+
+    Route::group([
+        'middleware' => 'role:student',
+        'prefix' => 'student',
+        'as' => 'student.'
+    ], function () {
+        Route::get('/', [HomeController::class, 'student'])->name('show');
+    });
+});
